1d8a563c92364e2db9cc7c0a708cd7ce9c4c83d2
ZackDev <devzacknmr28@gmail.com>
2024-12-08 19:11:43 -0600

    Refactor index.ts for improved code consistency and clarity

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import urlRoutes from './routes/urlsRoutes'
+import urlRoutes from '../src/routes/urlsRoutes';
 
-dotenv.config()
+const app = express();
+dotenv.config();
 
-const port = process.env.PORT || 3000
-const app = express()
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/shorten', urlRoutes)
+app.use('/shorten', urlRoutes);
 
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
 mongoose.connect(uri)
-.then(()=> console.log('Connected To DB'))
-.catch((e)=> console.log('Mongo db connection error: ', e))
+  .then(() => console.log('Connected To DB'))
+  .catch((e)=> console.log('Mongo db connection error: ', e));
 
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}: http://localhost:3000`)
-})
 
-export default app
+export default app;
